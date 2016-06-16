14048aa0dea18cea6b62062fe6085afb256579e7
Roel Bijlard <roel.101@live.nl>
2016-06-16 11:42:39 +0200

    fix voor merge error

--- app/src/main/java/com/example/roel/qstudent/Models/NavBar/NavBar.java
+++ app/src/main/java/com/example/roel/qstudent/Models/NavBar/NavBar.java
@@ -3,15 +3,12 @@ package com.example.roel.qstudent.Models.NavBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-<<<<<<< HEAD
 import android.util.Log;
-import android.view.View;
-=======
->>>>>>> sync
 
+import com.example.roel.qstudent.Activitys.MainActivity;
 import com.example.roel.qstudent.Activitys.ProfileActivity;
 import com.example.roel.qstudent.R;
 import com.roughike.bottombar.BottomBar;
@@ -20,15 +17,16 @@ import com.roughike.bottombar.OnMenuTabClickListener;
 public class NavBar extends AppCompatActivity implements OnMenuTabClickListener {
 
     private static BottomBar bar;
-    private boolean init=false;
+    private boolean init = false;
     private Context con;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     public void setupBar(Activity act, Bundle savedInstanceState) {
-        bar =BottomBar.attach(act, savedInstanceState);
+        bar = BottomBar.attach(act, savedInstanceState);
         //bar.useFixedMode();
 
         bar.setItemsFromMenu(R.menu.bottombar_menu, this);
@@ -36,17 +34,17 @@ public class NavBar extends AppCompatActivity implements OnMenuTabClickListener 
     }
 
     public void barLoaded(Activity act) {
-        if(act instanceof MainActivity) {
+        if (act instanceof MainActivity) {
             bar.selectTabAtPosition(0, false);
-            init=true;
-        } else if(act instanceof ProfileActivity) {
+            init = true;
+        } else if (act instanceof ProfileActivity) {
             bar.selectTabAtPosition(3, false);
-            init=true;
+            init = true;
         }
     }
 
     public void launchScreen(Class scr) {
-        if(this.getClass() == scr) {
+        if (this.getClass() == scr) {
             Log.d("[BAR]", "Same screen!");
             return;
         }
@@ -66,13 +64,13 @@ public class NavBar extends AppCompatActivity implements OnMenuTabClickListener 
 
     @Override
     public void onMenuTabSelected(@IdRes int menuItemId) {
-        if(!init) {
+        if (!init) {
             Log.d("[BAR]:", "init false");
             return;
         } else {
-            if(menuItemId == R.id.bottomBarItemOne) {
+            if (menuItemId == R.id.bottomBarItemOne) {
                 launchScreen(MainActivity.class);
-            }else if(menuItemId == R.id.bottomBarItemFour) {
+            } else if (menuItemId == R.id.bottomBarItemFour) {
                 launchScreen(ProfileActivity.class);
             }
         }
